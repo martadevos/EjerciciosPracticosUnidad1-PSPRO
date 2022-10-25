2b02ba7d3c192c32b3aaa03b8d1472a9c4eabb61
mdevos <martadevos@iesnervion.es>
2022-10-25 14:42:55 +0200

    25/10/2022

--- src/Ejercicio1/Ejercicio1.java
+++ src/Ejercicio1/Ejercicio1.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String ruta, nombre;
 		int opc = menu();
 
 		// Según la opción introducida debemos lanzar un proceso u otro.
@@ -14,14 +16,24 @@ public class Ejercicio1 {
 		// Si elige crear una carpeta, debo pedirle también la ruta donde quiere crearla
 		// y el nombre de la carpeta
 		case 1:
+			System.out.println("Introduzca la ruta donde desee crear la carpeta");
+			ruta = s.nextLine();
+			System.out.println("Introduzca el nombre de la carpeta");
+			nombre = s.nextLine();
 			break;
 		// Si elige crear un fichero, debo pedirle también la ruta donde quiere crearlo
 		// y el nombre del fichero
 		case 2:
+			System.out.println("Introduzca la ruta donde desee crear el fichero");
+			ruta = s.nextLine();
+			System.out.println("Introduzca el nombre del fichero");
+			nombre = s.nextLine();
 			break;
 		// Si elige mostrar un directorio, debo pedirle también la ruta del directorio a
 		// mostrar. Si lo deja vacío, debo mostrar el contenido del directorio actual
 		case 3:
+			System.out.println("Introduzca la ruta del directorio a mostrar o pulse intro para mostrar el directorio actual");
+			ruta = s.nextLine();
 			break;
 		default:
 			System.out.println("La opción introducida no es correcta");
